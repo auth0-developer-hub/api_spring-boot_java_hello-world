aa000c8c1a40581158a118215daf0e705f197a43
Jose Luis Leon <joseluis5000l@gmail.com>
2022-01-07 19:03:54 -0500

    Update message structure and text

--- src/main/java/com/example/helloworld/models/Message.java
+++ src/main/java/com/example/helloworld/models/Message.java
@@ -5,9 +5,26 @@ import lombok.Value;
 @Value
 public class Message {
 
+  private Metadata metadata;
+
   private String text;
 
   public static Message from(final String text) {
-    return new Message(text);
+    return new Message(Metadata.init(), text);
+  }
+
+  @Value
+  private static class Metadata {
+
+    private String api;
+
+    private String branch;
+
+    public static Metadata init() {
+      return new Metadata(
+        "api_spring-boot_java_hello-world",
+        "basic-role-based-access-control"
+      );
+    }
   }
 }
